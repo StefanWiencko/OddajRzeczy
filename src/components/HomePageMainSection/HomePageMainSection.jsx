b5b8e9ca0e02ced35ad1f42dba8d6f5c8004df79
Stefan Wiencko <stefanwiencko2@gmail.com>
2020-10-31 22:14:15 +0100

    adding bg images, finishing homepage rwd

--- src/components/HomePageMainSection/HomePageMainSection.jsx
+++ src/components/HomePageMainSection/HomePageMainSection.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import HomePageMainSectionNav from "../HomePageMainSectionNav/HomePageMainSectionNav"
-import MainOrnament from "../MainOrnament/MainOrnament"
-import HomePageMainSectionButtons from "../HomePageMainSectionButtons/HomePageMainSectionButtons"
+import React from "react";
+import HomePageMainSectionNav from "../HomePageMainSectionNav/HomePageMainSectionNav";
+import MainOrnament from "../MainOrnament/MainOrnament";
+import HomePageMainSectionButtons from "../HomePageMainSectionButtons/HomePageMainSectionButtons";
 
 function HomePageMainSection() {
-    return (
-        <section className="homePageMainSection">
-            <HomePageMainSectionNav/>
-            <MainOrnament text='Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce'/>
-            <HomePageMainSectionButtons/>
-        </section>
-    )
+  return (
+    <section className="homePageMainSection">
+      <div>
+        <HomePageMainSectionNav />
+        <MainOrnament text="Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce" />
+        <HomePageMainSectionButtons />
+      </div>
+
+      <div className="homePageMainSectionImg"></div>
+    </section>
+  );
 }
 
-export default HomePageMainSection
+export default HomePageMainSection;
